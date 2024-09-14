c9ef16bb62e13a9d25456a6491027c917754b383
Cezar Garrido Britez <cezar.britez@b2wdigital.com>
2024-09-13 22:15:29 -0300

    chore: rename to fundos

--- src/main.rs
+++ src/main.rs
@@ -28,9 +28,9 @@ async fn main() -> eframe::Result<()> {
         ..Default::default()
     };
     eframe::run_native(
-        "fundos",
+        "Fundos",
         native_options,
-        Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+        Box::new(|cc| Box::new(fundos::TemplateApp::new(cc))),
     )
 }
 
